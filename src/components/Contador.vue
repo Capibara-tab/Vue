63778a7eae04e6dc3bddf81bd108da3b5ead1b47
J.Marzano <joelmarzano06.06@gmail.com>
2025-02-14 08:36:02 -0300

    Creando componentes de contenedores

---
+++ src/components/Contador.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello World!</h1>
+  <p> { contador } </p>
+  <button> @click="incrementar()"</button>
+  <button>@click="decrementar()"</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+};
+const decrementar = () => {
+  contadpr.value--;
+};
+
+</script>
+
+<style>
+p {
+  color : red;
+}
+</style>
