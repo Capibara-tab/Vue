dcff25f9004a9ca6ec205e3ff5dfb725909a8c2f
J.Marzano <joelmarzano06.06@gmail.com>
2025-02-14 11:45:41 -0300

    Creando componentes de una Lista de Tareas. Borrar compponentes autogenerandos. Crea rutas

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-<Contador></Contador>
+<contador></contador>
+<listadetareas></listadetareas>
 </template>
 
 <script>
-import Contador from './componests/contador.vue'
+import Contador from './components/Contador.vue'
+import Listadetareas from './components/Listadetareas.vue'
 
 </script>
-
+import { ref} from 'vue'
+const tareas = ref([]);
+const nuevatarea = ref('');
 <style scoped>
 
 </style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,20 @@
+<template> 
+<div class="container">
+    <div class = "content">
+    
+    </div>
+
+</div>
+</template>
+<script setup>
+
+</script>
+<stlye>
+.container {
+    display: flex;
+}
+.content {
+    flex: 1;
+    padding: 20px;
+}
+</stlye>
